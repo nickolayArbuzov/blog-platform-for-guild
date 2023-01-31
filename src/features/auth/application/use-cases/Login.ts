3625cb409249caae960cbd19628e70dee7d913f0
Nickolay <arbuzov.n@f-case.ru>
2023-01-31 14:14:47 +0300

    feat

--- src/features/auth/application/use-cases/Login.ts
+++ src/features/auth/application/use-cases/Login.ts
@@ -39,13 +39,13 @@ export class LoginUseCase {
           title: command.deviceName, 
           deviceId: deviceId,
           issuedAt: new Date().getTime(),
-          expiresAt: new Date().getTime() + 600000,
+          expiresAt: new Date().getTime() + 300000,
           userId: auth.id!.toString(),
         }
         const payloadAccess = {userId: auth?.id?.toString() ? auth?.id?.toString() : '', userLogin: auth.login, deviceId: device.deviceId, issuedAt: device.issuedAt}
         const payloadRefresh = {userId: auth?.id?.toString() ? auth?.id?.toString() : '', userLogin: auth.login, deviceId: device.deviceId, issuedAt: device.issuedAt}
-        const accessToken = this.jwtService.sign(payloadAccess, {expiresIn: '5m'})
-        const refreshToken = this.jwtService.sign(payloadRefresh, {expiresIn: '10m'})
+        const accessToken = this.jwtService.sign(payloadAccess, {expiresIn: '2m'})
+        const refreshToken = this.jwtService.sign(payloadRefresh, {expiresIn: '5m'})
         await this.devicesRepo.createDevice(device)
         return {
           accessToken,
